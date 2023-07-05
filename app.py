c28b40f1bd1c3685a67a3480322210f820a164df
VNS-KO <79849717+VNS-KO@users.noreply.github.com>
2023-07-05 12:50:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+from langchain import OpenAI
+from langchain.agents import create_csv_agent
 import openai
 import pandas as pd
 import streamlit as st
-
-openai.api_key = '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 '
+from dotenv import load_dotenv
+import os
 
 
 def make_prediction(prompt):
@@ -18,40 +20,35 @@ def make_prediction(prompt):
     return response.choices[0].text.strip()
 
 
-def generate_predictions(file_path):
-    
-    try:
-        df = pd.read_csv(file_path, engine='openpyxl')
-        st.success("File successfully loaded!")
-    except Exception as e:
-        st.error(f"Error loading file: {e}")
-        return
-    
-    
-    for index, row in df.iterrows():
-        
-        input_data = row['input_column_name']
-        
-       
-        prediction = make_prediction(input_data)
-        
-        
-        st.write(f"Prediction for input {index + 1}: {prediction}")
-
-
-
-file_path = 'user_input.csv'
-
-
 def main():
+
+    load_dotenv()
+
+    if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
+        print("OPENAI_API_KEY is not set")
+        exit(1)
+    else:
+        print("OPENAI_API_KEY is set")
+    
     st.title("Excel Prediction Chatbot")
     
    
-    file = st.file_uploader("Upload Excel file", type=["csv"])
+    csv_file = st.file_uploader("Upload Excel file", type=["csv"])
     
-    if file is not None:
-        
-        generate_predictions(file.name)  
+    if csv_file is not None:
+    # Save the uploaded file to a temporary location
+        temp_file_path = "temp.csv"
+        with open(temp_file_path, "wb") as file:
+            file.write(csv_file.getvalue())
+
+        agent = create_csv_agent(
+            OpenAI(temperature=0), temp_file_path, verbose=True)
+
+        user_question = st.text_input("Enter your column name for prediction: ")
+
+        if user_question is not None and user_question != "":
+            with st.spinner(text="In progress..."):
+                st.write(agent.run(user_question))
     
 if __name__ == '__main__':
     main()
